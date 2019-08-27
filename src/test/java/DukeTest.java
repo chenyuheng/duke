7c89bda56625188148a09a2d9e13e4d8c2e04ed7
chenyuheng <pass@chenyuheng.cn>
2019-08-27 19:14:32 +0800

    A-Gradle

--- src/test/java/DukeTest.java
+++ src/test/java/DukeTest.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class DukeTest {
+    public static void main(String[] args) {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Test Hello from\n" + logo);
+    }
 }
