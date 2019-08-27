dd37031047cecaf12f1f4503db045b7a3dd19731
chenyuheng <pass@chenyuheng.cn>
2019-08-27 21:58:11 +0800

    fix small bugs

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,16 +13,20 @@ public class Duke {
                     output+=i+1+"."+taskList.get(i)+"\n";
                 output+=taskList.size()+"."+taskList.get(taskList.size()-1);
                 answer(output);
+                continue;
             }else if(input.equals("bye")){
                 answer("Bye. Hope to see you again soon!");
                 break;
-            }else if(input.substring(0,4).equals("done")){
-                taskList.get(Integer.parseInt(input.substring(5))-1).markAsDone();
-                answer("Nice! I've marked this task as done: \n"+taskList.get(Integer.parseInt(input.substring(5))-1));
-            }else {
-                taskList.add(new Task(input));
-                answer("added: "+input);
+            }else if(input.length()>=4){
+                if(input.substring(0,4).equals("done")){
+                    taskList.get(Integer.parseInt(input.substring(5))-1).markAsDone();
+                    answer("Nice! I've marked this task as done: \n"+taskList.get(Integer.parseInt(input.substring(5))-1));
+                    continue;
+                }
             }
+            taskList.add(new Task(input));
+            answer("added: "+input);
+
         }
     }
     public static void answer(String toPrint){
