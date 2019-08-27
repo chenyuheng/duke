23e7e864667296fed522ce953af8942ac29bbad5
chenyuheng <pass@chenyuheng.cn>
2019-08-27 20:00:41 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,30 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        answer("Hello! I'm Duke\nWhat can I do for you?");
+        Scanner in=new Scanner(System.in);
+        while(true){
+            String input=in.nextLine();
+            if(input.equals("bye")){
+                answer("Bye. Hope to see you again soon!");
+                break;
+            }
+            answer(input);
+        }
+    }
+    public static void answer(String toPrint){
+        String horizontalLine="\t____________________________________________________________";
+        System.out.println(horizontalLine);
+        toPrint="\t"+toPrint;
+        for(int i=0;i<toPrint.length();i++){
+            if(toPrint.charAt(i)=='\n') {
+                //System.out.println(toPrint.length()+" "+i+toPrint.substring(0, i) + "," + toPrint.substring(i, toPrint.length())+".");
+                toPrint = toPrint.substring(0, i+1) + "\t" + toPrint.substring(i+1, toPrint.length());
+
+            }
+        }
+        System.out.println(toPrint);
+        System.out.println(horizontalLine);
+
     }
 }
