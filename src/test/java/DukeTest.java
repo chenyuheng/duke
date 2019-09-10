3162a5b6c40dbeac2c2ecf5ec48746515aac1465
chenyuheng <pass@chenyuheng.cn>
2019-09-10 10:45:21 +0800

    Add JUnit tests (done this before, this commit added a dummy test method.

--- src/test/java/DukeTest.java
+++ src/test/java/DukeTest.java
@@ -1,14 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class DukeTest {
-    /***<p>
-     * Main method of the test project.</p>
-     * @param args command line arguments
-     */
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Test Hello from\n" + logo);
+    @Test
+    public void dummyTest(){
+        assertEquals(2, 2);
     }
 }
